bb4e48f4ea139231a1b0efe1c4ecf8038e389850
AleksandrosP38 <alexandrepacheco2000@hotmail.com>
2024-04-29 12:01:15 +0100

    buttons image

--- Java.js
+++ Java.js
@@ -1,27 +1,55 @@
 const buttonContainer = document.getElementById('buttonContainer');
 
-const buttonBoxTexts = {
-    'Game': `The game takes place in a bunker, in first person. The character is trapped in this location for initially unknown reasons and for an undetermined amount of time, but enough to sometimes drive him crazy. Your objective is to escape this bunker without reaching total insanity, using the items and information at your disposal that vary according to the character's two psychological states: normal state and state of madness.`,
-    'About': `The game introduces a non-standard way of solving puzzles, in which you need to explore the bunker between two psychological states with different items and information. The game vision is innovative and more immersive not only for the genre but in general in current video games. The game is made to be played with a keyboard and mouse on PC.`,
-    'Community': 'Join our vibrant community and engage with other members.',
-    'News': 'Stay updated with the latest news and announcements.',
-    'Patch Notes': 'Check out the latest patch notes and updates for our products.',
-    'Requirements': 'Learn about the system requirements for our games and applications.',
-    'Support': 'Need help? Get in touch with our support team for assistance.'
+const buttonBoxData = {
+    'Game': {
+        text: `The game takes place in a bunker, in first person. The character is trapped in this location for initially unknown reasons and for an undetermined amount of time, but enough to sometimes drive him crazy. Your objective is to escape this bunker without reaching total insanity, using the items and information at your disposal that vary according to the character's two psychological states: normal state and state of madness.`,
+        image: 'Images/Tape.png' // Path to the image for the 'Game' button
+    },
+    'About': {
+        text: `The game introduces a non-standard way of solving puzzles, in which you need to explore the bunker between two psychological states with different items and information. The game vision is innovative and more immersive not only for the genre but in general in current video games. The game is made to be played with a keyboard and mouse on PC.`,
+        image: 'Images/Tape.png' // Path to the image for the 'About' button
+    },
+    'Community': {
+        text: 'Join our vibrant community and engage with other members.',
+        image: 'Images/Tape.png' // Path to the image for the 'Community' button
+    },
+    'News': {
+        text: 'Stay updated with the latest news and announcements.',
+        image: 'Images/Tape.png' // Path to the image for the 'News' button
+    },
+    'Patch Notes': {
+        text: 'Check out the latest patch notes and updates for our products.',
+        image: 'Images/Tape.png' // Path to the image for the 'Patch Notes' button
+    },
+    'Requirements': {
+        text: 'Learn about the system requirements for our games and applications.',
+        image: 'Images/Tape.png' // Path to the image for the 'Requirements' button
+    },
+    'Support': {
+        text: 'Need help? Get in touch with our support team for assistance.',
+        image: 'Images/Tape.png' // Path to the image for the 'Support' button
+    }
 };
 
 let currentBoxText = '';
 
-Object.keys(buttonBoxTexts).forEach(text => {
+Object.keys(buttonBoxData).forEach(text => {
     const button = document.createElement('button');
-    button.textContent = text;
+    button.classList.add('button-with-image'); // Add a class to represent buttons with background images
+    button.style.backgroundImage = `url(${buttonBoxData[text].image})`; // Set background image of the button
+    
+    const buttonText = document.createElement('span');
+    buttonText.classList.add('button-text');
+    buttonText.textContent = text;
+
+    button.appendChild(buttonText);
     buttonContainer.appendChild(button);
     
     const lineBreak = document.createElement('br');
     buttonContainer.appendChild(lineBreak);
     
     button.addEventListener('click', () => {
-        if (buttonBoxTexts[text] !== currentBoxText) {
+        if (buttonBoxData[text].text !== currentBoxText) {
             document.querySelectorAll('.dynamic-box').forEach(box => box.remove());
             
             const newDarkBox = document.createElement('div');
@@ -63,7 +91,7 @@ Object.keys(buttonBoxTexts).forEach(text => {
             newTextContainer.classList.add('text-container');
 
             const newText = document.createElement('p');
-            newText.textContent = buttonBoxTexts[text];
+            newText.textContent = buttonBoxData[text].text;
             newText.style.textAlign = 'center';
             newText.style.fontSize = '2vw';
 
@@ -73,7 +101,7 @@ Object.keys(buttonBoxTexts).forEach(text => {
 
             document.body.appendChild(newDarkBox);
 
-            currentBoxText = buttonBoxTexts[text];
+            currentBoxText = buttonBoxData[text].text;
         }
     });
 });
